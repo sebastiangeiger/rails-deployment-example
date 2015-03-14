9497822d2079d55683f363194b94b8f506f83041
Sebastian Geiger <sebastian.geiger@gmail.com>
2015-03-13 20:27:31 -0400

    Adding a viewcounter

---
+++ Procfile
@@ -0,0 +1,2 @@
+rails: bundle exec rails server
+redis: redis-server
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    redis = Redis.new
+    @views = redis.get("views").to_i + 1
+    redis.set("views", @views)
   end
 end

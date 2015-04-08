442553981979018f6a50b1d7eb55ffce2308ce79
Sebastian Geiger <sebastian.geiger@gmail.com>
2015-04-07 20:46:29 -0400

    Removing the rails app, not part of deployment

--- db/migrate/20150314003309_create_arbitrary_counters.rb
+++
@@ -1,9 +0,0 @@
-class CreateArbitraryCounters < ActiveRecord::Migration
-  def change
-    create_table :arbitrary_counters do |t|
-      t.integer :counter, default: 0
-
-      t.timestamps null: false
-    end
-  end
-end

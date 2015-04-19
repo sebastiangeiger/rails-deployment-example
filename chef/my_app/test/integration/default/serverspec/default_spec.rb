fa2bf82c83d3a489ff13adb3d84921920287a04d
Sebastian Geiger <sebastian.geiger@gmail.com>
2015-04-18 20:32:17 -0400

    Making sure postgres 9.1 runs

--- chef/my_app/test/integration/default/serverspec/default_spec.rb
+++ chef/my_app/test/integration/default/serverspec/default_spec.rb
@@ -6,15 +6,23 @@ describe 'my_app::default' do
     its(:stdout) { should match /^ruby\ 2\.1\.6/ }
   end
 
-  describe port(5432) do
-    it { should be_listening }
-  end
+  describe "PostgreSQL" do
+    describe process("postgres") do
+      it { should be_running }
+      its(:cmd) { should include "/postgresql/9.1/bin/postgres\s" }
+      it 'runs 9.1' do
+        binary = subject.cmd.split(" ").first
+        version_query = "#{binary} --version"
+        expect(command(version_query).stdout).to match /9\.1\.\d+/
+      end
+    end
 
-  describe process("postgres") do
-    it { should be_running }
-  end
+    describe port(5432) do
+      it { should be_listening }
+    end
 
-  describe command('which psql') do
-    its(:exit_status) { should eq 0 }
+    describe command('which psql') do
+      its(:exit_status) { should eq 0 }
+    end
   end
 end

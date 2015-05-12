ed476bf350067d4d4827a091d5007f7d1731af77
Sebastian Geiger <sebastian.geiger@gmail.com>
2015-05-11 21:49:35 -0400

    Generated my_app_docker cookbook

---
+++ chef/my_app_docker/metadata.rb
@@ -0,0 +1,8 @@
+name             'my_app_docker'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures my_app_docker'
+long_description 'Installs/Configures my_app_docker'
+version          '0.1.0'
+
